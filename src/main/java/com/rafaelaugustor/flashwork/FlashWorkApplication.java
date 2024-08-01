99abd3b4b792ac59e7a2da8e6af3763a99f1fac9
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-08-01 20:33:18 -0300

    feat: initial commit

---
+++ src/main/java/com/rafaelaugustor/flashwork/FlashWorkApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelaugustor.flashwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelaugustor/flashwork/FlashWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelaugustor.flashwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
