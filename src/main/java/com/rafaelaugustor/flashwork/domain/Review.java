b8e8bd64e12a7f25ac968bbfa9a883cc73749ad0
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-08-03 21:38:17 -0300

    feat: creating entity Review

---
+++ src/main/java/com/rafaelaugustor/flashwork/domain/Review.java
@@ -0,0 +1,27 @@
+package com.rafaelaugustor.flashwork.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private Integer rating;
+
+    private String description;
+
+    private UUID targetId;
+
+
+}
