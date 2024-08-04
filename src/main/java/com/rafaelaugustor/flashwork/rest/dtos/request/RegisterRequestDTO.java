db64b68d9aa7713d5f0824a536c0aa20b5daddf2
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-08-04 19:18:47 -0300

    feat: creating RegisterRequestDTO

---
+++ src/main/java/com/rafaelaugustor/flashwork/rest/dtos/request/RegisterRequestDTO.java
@@ -0,0 +1,38 @@
+package com.rafaelaugustor.flashwork.rest.dtos.request;
+
+import com.rafaelaugustor.flashwork.domain.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegisterRequestDTO {
+
+    private String name;
+
+    private String cpf;
+
+    private String phone;
+
+    private String email;
+
+    private String profession;
+
+    private String description;
+
+    private LocalDate birthDate;
+
+    private String password;
+
+    private String confirmPassword;
+
+    private String profilePicture;
+
+    private UserRole role;
+}
