83834f4a51b4867f628e47e76cb8682ab5c0ef0a
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-08-04 21:08:33 -0300

    feat: adding endpoint for user login

--- src/main/java/com/rafaelaugustor/flashwork/rest/controllers/AuthController.java
+++ src/main/java/com/rafaelaugustor/flashwork/rest/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package com.rafaelaugustor.flashwork.rest.controllers;
 
+import com.rafaelaugustor.flashwork.rest.dtos.request.LoginRequestDTO;
 import com.rafaelaugustor.flashwork.rest.dtos.request.RegisterRequestDTO;
+import com.rafaelaugustor.flashwork.rest.dtos.response.LoginResponseDTO;
 import com.rafaelaugustor.flashwork.services.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,12 @@ import static com.rafaelaugustor.flashwork.utils.Constants.APP_ROOT;
 public class AuthController {
 
     private final AuthService service;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO request) {
+        LoginResponseDTO response = service.login(request);
+        return ResponseEntity.ok(response);
+    }
 
     @PostMapping("/register")
     public ResponseEntity<Void> register(@RequestBody RegisterRequestDTO request){
