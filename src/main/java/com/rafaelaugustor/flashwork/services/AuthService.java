cee5b549c58d86e3d6d943e909592d1f17911da4
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-08-04 21:08:05 -0300

    feat: adding login method to UserService and returning the token to the user

--- src/main/java/com/rafaelaugustor/flashwork/services/AuthService.java
+++ src/main/java/com/rafaelaugustor/flashwork/services/AuthService.java
@@ -3,8 +3,12 @@ package com.rafaelaugustor.flashwork.services;
 import com.rafaelaugustor.flashwork.domain.entities.User;
 import com.rafaelaugustor.flashwork.domain.enums.UserRole;
 import com.rafaelaugustor.flashwork.repositories.UserRepository;
+import com.rafaelaugustor.flashwork.rest.dtos.request.LoginRequestDTO;
 import com.rafaelaugustor.flashwork.rest.dtos.request.RegisterRequestDTO;
+import com.rafaelaugustor.flashwork.rest.dtos.response.LoginResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +17,18 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final UserRepository repository;
+    private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    public LoginResponseDTO login(LoginRequestDTO request) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
+
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+        return new LoginResponseDTO(token);
+    }
 
     public void register(RegisterRequestDTO request) {
         User user = repository.findByEmail(request.getEmail());
