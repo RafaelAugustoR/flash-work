faca36b4a4d2b485c4390693ba3b877059f1ec90
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-08-15 20:05:22 -0300

    feat: creating query findByIdAndProviderEmail in ServiceRepository

--- src/main/java/com/rafaelaugustor/flashwork/repositories/ServiceRepository.java
+++ src/main/java/com/rafaelaugustor/flashwork/repositories/ServiceRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ServiceRepository extends JpaRepository<Service, UUID> {
+    Service findByIdAndProviderEmail(UUID serviceId, String email);
 }
