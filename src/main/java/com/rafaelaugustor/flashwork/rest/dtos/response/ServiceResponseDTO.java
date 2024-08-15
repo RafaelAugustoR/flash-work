7502c2a64fd061536bd27d53a1464dd39574a685
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-08-15 20:04:37 -0300

    feat: adding ServiceResponseDTO

---
+++ src/main/java/com/rafaelaugustor/flashwork/rest/dtos/response/ServiceResponseDTO.java
@@ -0,0 +1,27 @@
+package com.rafaelaugustor.flashwork.rest.dtos.response;
+
+import com.rafaelaugustor.flashwork.domain.enums.WorkType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceResponseDTO {
+    private UUID id;
+    private String title;
+    private String description;
+    private Double price;
+    private WorkType workType;
+    private String location;
+    private Timestamp createdAt;
+    private UUID providerId;
+    private List<String> categories;
+}
