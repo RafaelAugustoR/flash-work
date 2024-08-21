3155c675b40525ceff2aaf6092766d1f8c37006e
jl-monteiro <jlcs1609@gmail.com>
2024-08-21 16:41:43 -0300

    feat: create query in ServiceRequestRepository

--- src/main/java/com/rafaelaugustor/flashwork/repositories/ServiceRequestRepository.java
+++ src/main/java/com/rafaelaugustor/flashwork/repositories/ServiceRequestRepository.java
@@ -3,7 +3,9 @@ package com.rafaelaugustor.flashwork.repositories;
 import com.rafaelaugustor.flashwork.domain.entities.ServiceRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ServiceRequestRepository extends JpaRepository<ServiceRequest, UUID> {
+    Optional<ServiceRequest> findByIdAndRequesterEmail(UUID requestId, String email);
 }
