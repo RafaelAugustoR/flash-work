8d51f9088345e255bdee0954e6036b4e03264f40
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-08-26 21:43:40 -0300

    feat: add query in ReviewRepository

--- src/main/java/com/rafaelaugustor/flashwork/repositories/ReviewRepository.java
+++ src/main/java/com/rafaelaugustor/flashwork/repositories/ReviewRepository.java
@@ -3,7 +3,9 @@ package com.rafaelaugustor.flashwork.repositories;
 import com.rafaelaugustor.flashwork.domain.entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
+    List<Review> findReviewsByTargetId(UUID targetId);
 }
