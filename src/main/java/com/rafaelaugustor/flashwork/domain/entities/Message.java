9770db95f914e4ae953a00db0f7ffd8605b7eac4
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-09-08 21:05:44 -0300

    feat: create entity Message

---
+++ src/main/java/com/rafaelaugustor/flashwork/domain/entities/Message.java
@@ -0,0 +1,38 @@
+package com.rafaelaugustor.flashwork.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Table(name = "messages")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+
+    private Timestamp sentAt;
+
+    private Timestamp readAt;
+}
