22a470f7dbda87ef50161eb5e2a3d0cc1e447236
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-09-24 13:54:13 -0300

    feat: adding constructor in UserResponseDTO

--- src/main/java/com/rafaelaugustor/flashwork/rest/dtos/response/UserResponseDTO.java
+++ src/main/java/com/rafaelaugustor/flashwork/rest/dtos/response/UserResponseDTO.java
@@ -35,4 +35,10 @@ public class UserResponseDTO {
     private LocalDate birthDate;
 
     private UserRole role;
+
+    public UserResponseDTO(UUID id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
