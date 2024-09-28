c4e0aed1eba99b235682ce9c5c50e6a69860bc2e
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-09-28 13:02:48 -0300

    feat: add field sender in NotificationRequestDTO

--- src/main/java/com/rafaelaugustor/flashwork/rest/dtos/request/NotificationRequestDTO.java
+++ src/main/java/com/rafaelaugustor/flashwork/rest/dtos/request/NotificationRequestDTO.java
@@ -1,6 +1,5 @@
 package com.rafaelaugustor.flashwork.rest.dtos.request;
 
-import com.rafaelaugustor.flashwork.domain.enums.NotificationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +12,6 @@ import java.util.UUID;
 public class NotificationRequestDTO {
 
     private String content;
-    private NotificationType type;
     private UUID receiver;
+    private UUID sender;
 }
