a80992f8ca23a670e69afb797055e631fba4bf9f
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-09-29 20:33:21 -0300

    refactor: change param of the method create in ChatController

--- src/main/java/com/rafaelaugustor/flashwork/rest/controllers/ChatController.java
+++ src/main/java/com/rafaelaugustor/flashwork/rest/controllers/ChatController.java
@@ -2,6 +2,7 @@ package com.rafaelaugustor.flashwork.rest.controllers;
 
 import com.rafaelaugustor.flashwork.domain.entities.User;
 import com.rafaelaugustor.flashwork.repositories.UserRepository;
+import com.rafaelaugustor.flashwork.rest.dtos.request.ChatRequestDTO;
 import com.rafaelaugustor.flashwork.rest.dtos.response.ChatResponseDTO;
 import com.rafaelaugustor.flashwork.services.ChatService;
 import lombok.RequiredArgsConstructor;
@@ -16,17 +17,17 @@ import static com.rafaelaugustor.flashwork.utils.Constants.APP_ROOT;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(APP_ROOT + "/chat")
+@RequestMapping(APP_ROOT + "/chats")
 @CrossOrigin("*")
 public class ChatController {
 
     private final ChatService chatService;
     private final UserRepository userRepository;
 
-    @PostMapping("/create")
-    public ResponseEntity<Void> createChat(@RequestBody UUID otherUserId, Principal principal) {
+    @PostMapping
+    public ResponseEntity<Void> createChat(@RequestBody ChatRequestDTO request, Principal principal) {
         User userOne = userRepository.findByEmail(principal.getName());
-        User userTwo = userRepository.findById(otherUserId).orElseThrow(() -> new RuntimeException("User not found"));
+        User userTwo = userRepository.findById(request.getUserId()).orElseThrow(() -> new RuntimeException("User not found"));
         chatService.createChat(userOne, userTwo);
         return ResponseEntity.ok().build();
     }
