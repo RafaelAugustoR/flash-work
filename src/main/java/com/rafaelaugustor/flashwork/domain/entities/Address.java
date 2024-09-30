ae80bbb37b6b54cbd7bde7f7510069f66637df56
rafaelaugustor <rafaelaugustodev@gmail.com>
2024-09-30 14:10:22 -0300

    feat: create entity Address

---
+++ src/main/java/com/rafaelaugustor/flashwork/domain/entities/Address.java
@@ -0,0 +1,57 @@
+package com.rafaelaugustor.flashwork.domain.entities;
+
+import com.rafaelaugustor.flashwork.domain.enums.AddressType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = true, length = 50)
+    private String country;
+
+    @Column(nullable = false, length = 100)
+    private String city;
+
+    @Column(nullable = false, length = 50)
+    private String state;
+
+    @Column(nullable = false, length = 50)
+    private String neighborhood;
+
+    @Column(nullable = false, length = 50)
+    private String street;
+
+    @Column(nullable = false, length = 20)
+    private String postalCode;
+
+    @Column(nullable = false)
+    private Integer houseNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AddressType type;
+
+    @Column(nullable = true)
+    private Integer apartmentNumber;
+
+    @Column(nullable = true, length = 50)
+    private String apartmentName;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
