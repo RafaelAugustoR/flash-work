2a0ccc5ecf2c65e3fd621591910b84838af8b956
rafaelaugustor <rafaelaugustodev@gmail.com>
2024-10-16 14:33:05 -0300

    feat: add PixPaymentResponseDTO

---
+++ src/main/java/com/rafaelaugustor/flashwork/rest/dtos/response/PixPaymentResponseDTO.java
@@ -0,0 +1,17 @@
+package com.rafaelaugustor.flashwork.rest.dtos.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class PixPaymentResponseDTO {
+    private Long id;
+    private String status;
+    private String detail;
+    private String qrCodeBase64;
+    private String qrCode;
+
+}
