fa1e691fc964bd317a023df37746e8ff49752b1a
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-10-17 20:09:27 -0300

    feat: create PaymentController to expose endpoint

---
+++ src/main/java/com/rafaelaugustor/flashwork/rest/controllers/PaymentController.java
@@ -0,0 +1,35 @@
+package com.rafaelaugustor.flashwork.rest.controllers;
+
+import com.rafaelaugustor.flashwork.rest.dtos.request.PixPaymentRequestDTO;
+import com.rafaelaugustor.flashwork.rest.dtos.response.PixPaymentResponseDTO;
+import com.rafaelaugustor.flashwork.services.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+import static com.rafaelaugustor.flashwork.utils.Constants.APP_ROOT;
+
+@RestController
+@RequestMapping(APP_ROOT + "/process_payment")
+@RequiredArgsConstructor
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    @PostMapping("/pix")
+    public ResponseEntity<PixPaymentResponseDTO> processPixPayment(@RequestBody PixPaymentRequestDTO pixPaymentDTO, Principal principal) {
+        PixPaymentResponseDTO payment = paymentService.processPixPayment(pixPaymentDTO, principal);
+        return ResponseEntity.status(HttpStatus.CREATED).body(payment);
+    }
+
+//    @PostMapping("/card")
+//    public ResponseEntity<Void> processCardPayment(@RequestBody CardPaymentRequestDTO request) {
+//        paymentService.processCardPayment(request);
+//        return ResponseEntity.ok().build();
+//    }
+}
