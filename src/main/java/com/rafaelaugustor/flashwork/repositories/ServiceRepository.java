c279961f7cb0415b3ce75909c104350887897ead
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-10-22 15:43:54 -0300

    feat: add queries in ServiceRepository

--- src/main/java/com/rafaelaugustor/flashwork/repositories/ServiceRepository.java
+++ src/main/java/com/rafaelaugustor/flashwork/repositories/ServiceRepository.java
@@ -1,7 +1,10 @@
 package com.rafaelaugustor.flashwork.repositories;
 
 import com.rafaelaugustor.flashwork.domain.entities.Service;
+import com.rafaelaugustor.flashwork.domain.enums.WorkType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +17,9 @@ public interface ServiceRepository extends JpaRepository<Service, UUID> {
     List<Service> findByClientEmail(String email);
 
     List<Service> findByCategoriesId(UUID categoryId);
+
+    List<Service> findByWorkType(WorkType workType);
+
+    @Query("SELECT s FROM Service s WHERE LOWER(s.location) = LOWER(:location)")
+    List<Service> findByLocationExcludingClient(@Param("location") String location);
 }
