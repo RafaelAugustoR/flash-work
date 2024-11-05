03262ea569a0c31afef2f2f05337d583f2a55889
rafaelaugustor <rafaelaugustodev@gmail.com>
2024-11-05 08:54:56 -0300

    feat: expose endpoints for the new methods

--- src/main/java/com/rafaelaugustor/flashwork/rest/controllers/ProposalController.java
+++ src/main/java/com/rafaelaugustor/flashwork/rest/controllers/ProposalController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.UUID;
 
 import static com.rafaelaugustor.flashwork.utils.Constants.APP_ROOT;
@@ -36,9 +37,29 @@ public class ProposalController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/user")
+    public ResponseEntity<List<ProposalResponseDTO>> findAllByUser(Principal principal){
+        var proposals = proposalService.findAllByUser(principal);
+
+        return ResponseEntity.ok().body(proposals);
+    }
+
+    @GetMapping("/service/{id}")
+    public ResponseEntity<List<ProposalResponseDTO>> findAllByService(@PathVariable UUID id){
+        var proposals = proposalService.findAllByService(id);
+
+        return ResponseEntity.ok().body(proposals);
+    }
+
     @PutMapping("/{id}/status")
     public ResponseEntity<ProposalResponseDTO> updateProposalStatus(@PathVariable UUID id, @RequestParam ProposalStatus status, Principal principal) {
         var proposal = proposalService.update(id, status, principal);
         return ResponseEntity.ok().body(proposal);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> cancelProposal(@PathVariable UUID id, Principal principal) {
+        proposalService.cancelProposal(id, principal);
+        return ResponseEntity.ok().build();
     }
 }
