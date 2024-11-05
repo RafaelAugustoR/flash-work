921d37957f941137b1db26b7bb85d24c5b3526b5
rafaelaugustor <rafaelaugustodev@gmail.com>
2024-11-05 08:52:38 -0300

    feat: add queries in ProposalRepository

--- src/main/java/com/rafaelaugustor/flashwork/repositories/ProposalRepository.java
+++ src/main/java/com/rafaelaugustor/flashwork/repositories/ProposalRepository.java
@@ -4,9 +4,13 @@ import com.rafaelaugustor.flashwork.domain.entities.Proposal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProposalRepository extends JpaRepository<Proposal, UUID> {
+
+    List<Proposal> findAllByFreelancerEmail(String email);
+
+    List<Proposal> findAllByServiceId(UUID serviceId);
 }
