304c9e6e18aabd8d5dd40b7450536d0b8d5b826f
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-11-14 20:58:57 -0300

    refactor: refactoring project structure and splitting imports

---
+++ src/main/resources/db/imports/004_category_service_seed.sql
@@ -0,0 +1,73 @@
+-- Serviços do cliente ID 317201ee-0bc6-45f1-bbfc-058f2d2a318a
+INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', 'd1e8a4b1-0b87-4d69-b9f4-df88b6005b67'); -- Manutenção
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', 'd1e8a4b1-0b87-4d69-b9f4-df88b6005b67'); -- Tecnologia
+INSERT INTO category_service (category_id, service_id) VALUES ('47cb792b-75cf-470a-94fa-76be1a0298c4', 'd1e8a4b1-0b87-4d69-b9f4-df88b6005b67'); -- Consultoria
+
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', 'f2b72f3d-0f02-4b47-8a3c-b6c3d7c2f5e4'); -- Tecnologia
+INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', 'f2b72f3d-0f02-4b47-8a3c-b6c3d7c2f5e4'); -- Design
+INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', 'f2b72f3d-0f02-4b47-8a3c-b6c3d7c2f5e4'); -- Manutenção
+
+INSERT INTO category_service (category_id, service_id) VALUES ('47cb792b-75cf-470a-94fa-76be1a0298c4', '3cb8c82c-ec06-43b3-8e3a-1f9d1f4d90f7'); -- Consultoria
+INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', '3cb8c82c-ec06-43b3-8e3a-1f9d1f4d90f7'); -- Engenharia
+INSERT INTO category_service (category_id, service_id) VALUES ('b14cdca0-48d4-4264-b13c-b658c92f9d0b', '3cb8c82c-ec06-43b3-8e3a-1f9d1f4d90f7'); -- Beleza
+
+-- Serviços do cliente ID 50c47e87-873a-4d82-8351-2b041123460a
+INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', '7c9c0c26-fc77-48f6-8729-754d5f3e5715'); -- Manutenção
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', '7c9c0c26-fc77-48f6-8729-754d5f3e5715'); -- Tecnologia
+INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', '7c9c0c26-fc77-48f6-8729-754d5f3e5715'); -- Engenharia
+
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', 'd6c8c60e-5d5c-4b3d-bfd9-c34c6ef06249'); -- Tecnologia
+INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', 'd6c8c60e-5d5c-4b3d-bfd9-c34c6ef06249'); -- Marketing
+INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', 'd6c8c60e-5d5c-4b3d-bfd9-c34c6ef06249'); -- Design
+
+INSERT INTO category_service (category_id, service_id) VALUES ('50ba3bb8-3b23-4d49-8f29-17af99c7403d', '1e8b83b9-8c82-45ab-bd09-43bfa1eb153b'); -- Contabilidade
+INSERT INTO category_service (category_id, service_id) VALUES ('7b8dcde3-50a1-4098-9382-03b61f5479cd', '1e8b83b9-8c82-45ab-bd09-43bfa1eb153b'); -- Administração
+INSERT INTO category_service (category_id, service_id) VALUES ('47cb792b-75cf-470a-94fa-76be1a0298c4', '1e8b83b9-8c82-45ab-bd09-43bfa1eb153b'); -- Consultoria
+
+-- Serviços do cliente ID d7062553-0a15-4ae1-9030-ab043c5c5a3c
+INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', '1e4d120c-26e7-4e8c-88f7-27b314de8b2e'); -- Manutenção
+INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', '1e4d120c-26e7-4e8c-88f7-27b314de8b2e'); -- Engenharia
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', '1e4d120c-26e7-4e8c-88f7-27b314de8b2e'); -- Tecnologia
+
+INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', 'c5f1b298-3e79-4e3e-b81a-bd50f8a5b24b'); -- Manutenção
+INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', 'c5f1b298-3e79-4e3e-b81a-bd50f8a5b24b'); -- Engenharia
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', 'c5f1b298-3e79-4e3e-b81a-bd50f8a5b24b'); -- Tecnologia
+
+INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', '28f34c26-93c3-4f39-bd1f-bc6b61f4f768'); -- Manutenção
+INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', '28f34c26-93c3-4f39-bd1f-bc6b61f4f768'); -- Engenharia
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', '28f34c26-93c3-4f39-bd1f-bc6b61f4f768'); -- Tecnologia
+
+-- Serviços do cliente ID 317201ee-0bc6-45f1-bbfc-058f2d2a318a (continuando)
+INSERT INTO category_service (category_id, service_id) VALUES ('7b8dcde3-50a1-4098-9382-03b61f5479cd', '8e3de7bc-f8cc-48b6-a54d-30e1e7b3f9b7'); -- Administração
+INSERT INTO category_service (category_id, service_id) VALUES ('47cb792b-75cf-470a-94fa-76be1a0298c4', '8e3de7bc-f8cc-48b6-a54d-30e1e7b3f9b7'); -- Consultoria
+INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', '8e3de7bc-f8cc-48b6-a54d-30e1e7b3f9b7'); -- Design
+
+INSERT INTO category_service (category_id, service_id) VALUES ('50ba3bb8-3b23-4d49-8f29-17af99c7403d', 'f84f1f59-f1c1-4f63-b4de-7cbe3252d68e'); -- Contabilidade
+INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', 'f84f1f59-f1c1-4f63-b4de-7cbe3252d68e'); -- Engenharia
+INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', 'f84f1f59-f1c1-4f63-b4de-7cbe3252d68e'); -- Manutenção
+
+INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', '5bc45120-31e1-4c72-ae62-b7f4c9ff646e'); -- Design
+INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', '5bc45120-31e1-4c72-ae62-b7f4c9ff646e'); -- Marketing
+INSERT INTO category_service (category_id, service_id) VALUES ('7b8dcde3-50a1-4098-9382-03b61f5479cd', '5bc45120-31e1-4c72-ae62-b7f4c9ff646e'); -- Administração
+
+-- Serviços do cliente ID 50c47e87-873a-4d82-8351-2b041123460a (continuando)
+INSERT INTO category_service (category_id, service_id) VALUES ('50ba3bb8-3b23-4d49-8f29-17af99c7403d', 'd3e8c527-0c94-4f44-8055-512e8c57c6dc'); -- Contabilidade
+INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', 'd3e8c527-0c94-4f44-8055-512e8c57c6dc'); -- Marketing
+INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', 'd3e8c527-0c94-4f44-8055-512e8c57c6dc'); -- Engenharia
+
+INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', 'e1d0b0b7-bf91-473b-8bff-d4b473d77b19'); -- Design
+INSERT INTO category_service (category_id, service_id) VALUES ('7b8dcde3-50a1-4098-9382-03b61f5479cd', 'e1d0b0b7-bf91-473b-8bff-d4b473d77b19'); -- Administração
+INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', 'e1d0b0b7-bf91-473b-8bff-d4b473d77b19'); -- Marketing
+
+-- Serviços do cliente ID d7062553-0a15-4ae1-9030-ab043c5c5a3c (continuando)
+INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', '4e2f1b42-3b43-4e4e-9c3b-1d2a4d6b76a9'); -- Design
+INSERT INTO category_service (category_id, service_id) VALUES ('47cb792b-75cf-470a-94fa-76be1a0298c4', '4e2f1b42-3b43-4e4e-9c3b-1d2a4d6b76a9'); -- Consultoria
+INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', '4e2f1b42-3b43-4e4e-9c3b-1d2a4d6b76a9'); -- Engenharia
+
+INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', 'a9b4a2e7-f66c-4c89-88f2-59d66fefb6cb'); -- Manutenção
+INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', 'a9b4a2e7-f66c-4c89-88f2-59d66fefb6cb'); -- Engenharia
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', 'a9b4a2e7-f66c-4c89-88f2-59d66fefb6cb'); -- Tecnologia
+
+INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', 'e6e1d5c3-0ed9-4478-aacd-bdfc3b0deaf2'); -- Marketing
+INSERT INTO category_service (category_id, service_id) VALUES ('7b8dcde3-50a1-4098-9382-03b61f5479cd', 'e6e1d5c3-0ed9-4478-aacd-bdfc3b0deaf2'); -- Administração
+INSERT INTO category_service (category_id, service_id) VALUES ('50ba3bb8-3b23-4d49-8f29-17af99c7403d', 'e6e1d5c3-0ed9-4478-aacd-bdfc3b0deaf2'); -- Contabilidade
