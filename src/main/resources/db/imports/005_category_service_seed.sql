7e17af45ea3031acd832a43f0663e76bfa56c7ca
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-11-23 16:10:49 -0300

    refactor: update imports

--- src/main/resources/db/imports/005_category_service_seed.sql
+++ src/main/resources/db/imports/005_category_service_seed.sql
@@ -1,73 +1,100 @@
--- Serviços do cliente ID 317201ee-0bc6-45f1-bbfc-058f2d2a318a
-INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', 'd1e8a4b1-0b87-4d69-b9f4-df88b6005b67'); -- Manutenção
-INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', 'd1e8a4b1-0b87-4d69-b9f4-df88b6005b67'); -- Tecnologia
-INSERT INTO category_service (category_id, service_id) VALUES ('47cb792b-75cf-470a-94fa-76be1a0298c4', 'd1e8a4b1-0b87-4d69-b9f4-df88b6005b67'); -- Consultoria
 
-INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', 'f2b72f3d-0f02-4b47-8a3c-b6c3d7c2f5e4'); -- Tecnologia
-INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', 'f2b72f3d-0f02-4b47-8a3c-b6c3d7c2f5e4'); -- Design
-INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', 'f2b72f3d-0f02-4b47-8a3c-b6c3d7c2f5e4'); -- Manutenção
 
-INSERT INTO category_service (category_id, service_id) VALUES ('47cb792b-75cf-470a-94fa-76be1a0298c4', '3cb8c82c-ec06-43b3-8e3a-1f9d1f4d90f7'); -- Consultoria
-INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', '3cb8c82c-ec06-43b3-8e3a-1f9d1f4d90f7'); -- Engenharia
-INSERT INTO category_service (category_id, service_id) VALUES ('b14cdca0-48d4-4264-b13c-b658c92f9d0b', '3cb8c82c-ec06-43b3-8e3a-1f9d1f4d90f7'); -- Beleza
+-- Service ID: 81f1c78b-79b8-43ba-b4c9-80c2a0c38edc
+INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', 'cfc26888-20d4-4146-83e5-d98ffc2d4642');
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', 'cfc26888-20d4-4146-83e5-d98ffc2d4642');
 
--- Serviços do cliente ID 50c47e87-873a-4d82-8351-2b041123460a
-INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', '7c9c0c26-fc77-48f6-8729-754d5f3e5715'); -- Manutenção
-INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', '7c9c0c26-fc77-48f6-8729-754d5f3e5715'); -- Tecnologia
-INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', '7c9c0c26-fc77-48f6-8729-754d5f3e5715'); -- Engenharia
+-- Service ID: 7a3cf297-b24d-45f7-8e3e-09e5e1c3d7d5
+INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', 'dab38234-6f41-4ed7-b1b9-6227a8be7da6');
+INSERT INTO category_service (category_id, service_id) VALUES ('5ab1ef10-65c0-4b98-b2b3-e305c8de9e43', 'dab38234-6f41-4ed7-b1b9-6227a8be7da6');
 
-INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', 'd6c8c60e-5d5c-4b3d-bfd9-c34c6ef06249'); -- Tecnologia
-INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', 'd6c8c60e-5d5c-4b3d-bfd9-c34c6ef06249'); -- Marketing
-INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', 'd6c8c60e-5d5c-4b3d-bfd9-c34c6ef06249'); -- Design
+-- Service ID: d61475a8-9462-4d0e-908e-1b1f32e2a6d9
+INSERT INTO category_service (category_id, service_id) VALUES ('1246f8d1-5bdf-4b9c-b7e4-49e3afc55ba8', 'ecef1ade-75a1-45c9-9140-1772a1a0dcf3');
+INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', 'ecef1ade-75a1-45c9-9140-1772a1a0dcf3');
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', 'ecef1ade-75a1-45c9-9140-1772a1a0dcf3');
 
-INSERT INTO category_service (category_id, service_id) VALUES ('50ba3bb8-3b23-4d49-8f29-17af99c7403d', '1e8b83b9-8c82-45ab-bd09-43bfa1eb153b'); -- Contabilidade
-INSERT INTO category_service (category_id, service_id) VALUES ('7b8dcde3-50a1-4098-9382-03b61f5479cd', '1e8b83b9-8c82-45ab-bd09-43bfa1eb153b'); -- Administração
-INSERT INTO category_service (category_id, service_id) VALUES ('47cb792b-75cf-470a-94fa-76be1a0298c4', '1e8b83b9-8c82-45ab-bd09-43bfa1eb153b'); -- Consultoria
+-- Service ID: b49eb4a6-3488-419d-915f-ea2fdc637c3d
+INSERT INTO category_service (category_id, service_id) VALUES ('7b8dcde3-50a1-4098-9382-03b61f5479cd', '0937170f-329b-4be5-b660-2fc42751b280');
+INSERT INTO category_service (category_id, service_id) VALUES ('47cb792b-75cf-470a-94fa-76be1a0298c4', '0937170f-329b-4be5-b660-2fc42751b280');
+INSERT INTO category_service (category_id, service_id) VALUES ('50ba3bb8-3b23-4d49-8f29-17af99c7403d', '0937170f-329b-4be5-b660-2fc42751b280');
 
--- Serviços do cliente ID d7062553-0a15-4ae1-9030-ab043c5c5a3c
-INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', '1e4d120c-26e7-4e8c-88f7-27b314de8b2e'); -- Manutenção
-INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', '1e4d120c-26e7-4e8c-88f7-27b314de8b2e'); -- Engenharia
-INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', '1e4d120c-26e7-4e8c-88f7-27b314de8b2e'); -- Tecnologia
+-- Service ID: e7c3af37-9678-49c9-9e4d-1d8c17d77c46
+INSERT INTO category_service (category_id, service_id) VALUES ('1246f8d1-5bdf-4b9c-b7e4-49e3afc55ba8', 'b5a81778-14f9-4b57-a8c5-ab9b71b8029c');
+INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', 'b5a81778-14f9-4b57-a8c5-ab9b71b8029c');
 
-INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', 'c5f1b298-3e79-4e3e-b81a-bd50f8a5b24b'); -- Manutenção
-INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', 'c5f1b298-3e79-4e3e-b81a-bd50f8a5b24b'); -- Engenharia
-INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', 'c5f1b298-3e79-4e3e-b81a-bd50f8a5b24b'); -- Tecnologia
+-- Service ID: 91c2eb6d-5c7f-44d9-89df-d905c8e4c4a2
+INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', '0ed04005-cc81-4032-92ce-4493447e1cec');
+INSERT INTO category_service (category_id, service_id) VALUES ('dbfdf6fa-34d5-49f8-9f6c-4b31a5dc57ef', '0ed04005-cc81-4032-92ce-4493447e1cec');
 
-INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', '28f34c26-93c3-4f39-bd1f-bc6b61f4f768'); -- Manutenção
-INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', '28f34c26-93c3-4f39-bd1f-bc6b61f4f768'); -- Engenharia
-INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', '28f34c26-93c3-4f39-bd1f-bc6b61f4f768'); -- Tecnologia
+-- Service ID: 4f9135f5-d97f-499e-8c97-9d2dc3c78e65
+INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', '8bb80fdf-5ddb-4dc7-bce0-970b9661579b');
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', '8bb80fdf-5ddb-4dc7-bce0-970b9661579b');
 
--- Serviços do cliente ID 317201ee-0bc6-45f1-bbfc-058f2d2a318a (continuando)
-INSERT INTO category_service (category_id, service_id) VALUES ('7b8dcde3-50a1-4098-9382-03b61f5479cd', '8e3de7bc-f8cc-48b6-a54d-30e1e7b3f9b7'); -- Administração
-INSERT INTO category_service (category_id, service_id) VALUES ('47cb792b-75cf-470a-94fa-76be1a0298c4', '8e3de7bc-f8cc-48b6-a54d-30e1e7b3f9b7'); -- Consultoria
-INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', '8e3de7bc-f8cc-48b6-a54d-30e1e7b3f9b7'); -- Design
+-- Service ID: 0fb87e37-98e8-4b3c-88de-77b50a88f5bc
+INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', '4481f0a1-394e-4445-b955-fc79bbca1160');
 
-INSERT INTO category_service (category_id, service_id) VALUES ('50ba3bb8-3b23-4d49-8f29-17af99c7403d', 'f84f1f59-f1c1-4f63-b4de-7cbe3252d68e'); -- Contabilidade
-INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', 'f84f1f59-f1c1-4f63-b4de-7cbe3252d68e'); -- Engenharia
-INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', 'f84f1f59-f1c1-4f63-b4de-7cbe3252d68e'); -- Manutenção
+-- Service ID: 1f24e82a-73ed-4902-870f-45f69e6cc8d8
+INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', '8d8b8884-5831-4dfa-b2c0-1df179ac3477');
+INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', '8d8b8884-5831-4dfa-b2c0-1df179ac3477');
 
-INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', '5bc45120-31e1-4c72-ae62-b7f4c9ff646e'); -- Design
-INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', '5bc45120-31e1-4c72-ae62-b7f4c9ff646e'); -- Marketing
-INSERT INTO category_service (category_id, service_id) VALUES ('7b8dcde3-50a1-4098-9382-03b61f5479cd', '5bc45120-31e1-4c72-ae62-b7f4c9ff646e'); -- Administração
+-- Service ID: 03ab55f7-402c-4e60-a2e5-9d124c65e251
+INSERT INTO category_service (category_id, service_id) VALUES ('7b8dcde3-50a1-4098-9382-03b61f5479cd', '0f5c2f07-5bf6-4dd3-9120-7c894212d3ee');
+INSERT INTO category_service (category_id, service_id) VALUES ('e8b123a3-4a29-4d47-bb1f-01c43e78f0d5', '0f5c2f07-5bf6-4dd3-9120-7c894212d3ee');
+INSERT INTO category_service (category_id, service_id) VALUES ('6a6d98c3-45c1-4f64-9e3f-3e6d43c3f6c6', '0f5c2f07-5bf6-4dd3-9120-7c894212d3ee');
 
--- Serviços do cliente ID 50c47e87-873a-4d82-8351-2b041123460a (continuando)
-INSERT INTO category_service (category_id, service_id) VALUES ('50ba3bb8-3b23-4d49-8f29-17af99c7403d', 'd3e8c527-0c94-4f44-8055-512e8c57c6dc'); -- Contabilidade
-INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', 'd3e8c527-0c94-4f44-8055-512e8c57c6dc'); -- Marketing
-INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', 'd3e8c527-0c94-4f44-8055-512e8c57c6dc'); -- Engenharia
+-- Service ID: 7d89c7f8-d491-4ba1-aeb9-67f6f3e91232
+INSERT INTO category_service (category_id, service_id) VALUES ('7b8dcde3-50a1-4098-9382-03b61f5479cd', 'f9b00247-564c-4693-b0cd-397de71bae52');
+INSERT INTO category_service (category_id, service_id) VALUES ('47cb792b-75cf-470a-94fa-76be1a0298c4', 'f9b00247-564c-4693-b0cd-397de71bae52');
+INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', 'f9b00247-564c-4693-b0cd-397de71bae52');
 
-INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', 'e1d0b0b7-bf91-473b-8bff-d4b473d77b19'); -- Design
-INSERT INTO category_service (category_id, service_id) VALUES ('7b8dcde3-50a1-4098-9382-03b61f5479cd', 'e1d0b0b7-bf91-473b-8bff-d4b473d77b19'); -- Administração
-INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', 'e1d0b0b7-bf91-473b-8bff-d4b473d77b19'); -- Marketing
+-- Service ID: b9f5c6a7-483e-4905-95d4-ea927b74853e
+INSERT INTO category_service (category_id, service_id) VALUES ('bf34c6b0-5dcb-4c2d-8d2c-e90950e6e91f', '77203ef2-83bf-45d7-92b9-21fa4867b6b8');
+INSERT INTO category_service (category_id, service_id) VALUES ('6a6d98c3-45c1-4f64-9e3f-3e6d43c3f6c6', '77203ef2-83bf-45d7-92b9-21fa4867b6b8');
 
--- Serviços do cliente ID d7062553-0a15-4ae1-9030-ab043c5c5a3c (continuando)
-INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', '4e2f1b42-3b43-4e4e-9c3b-1d2a4d6b76a9'); -- Design
-INSERT INTO category_service (category_id, service_id) VALUES ('47cb792b-75cf-470a-94fa-76be1a0298c4', '4e2f1b42-3b43-4e4e-9c3b-1d2a4d6b76a9'); -- Consultoria
-INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', '4e2f1b42-3b43-4e4e-9c3b-1d2a4d6b76a9'); -- Engenharia
+-- Service ID: af56d84d-4735-49d1-8d86-b27841a34d74
+INSERT INTO category_service (category_id, service_id) VALUES ('a49e4f41-4f7f-49cb-b38c-47620894b509', '8375c33c-956d-4cbc-a52c-2c9aff694121');
 
-INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', 'a9b4a2e7-f66c-4c89-88f2-59d66fefb6cb'); -- Manutenção
-INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', 'a9b4a2e7-f66c-4c89-88f2-59d66fefb6cb'); -- Engenharia
-INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', 'a9b4a2e7-f66c-4c89-88f2-59d66fefb6cb'); -- Tecnologia
+-- Service ID: 1c3b47ae-ff48-493b-bf2a-5879c73a4e31
+INSERT INTO category_service (category_id, service_id) VALUES ('6a6d98c3-45c1-4f64-9e3f-3e6d43c3f6c6', '0ea3c8cb-dd74-4bd1-8875-9974a9a10e90');
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', '0ea3c8cb-dd74-4bd1-8875-9974a9a10e90');
+INSERT INTO category_service (category_id, service_id) VALUES ('bf34c6b0-5dcb-4c2d-8d2c-e90950e6e91f', '0ea3c8cb-dd74-4bd1-8875-9974a9a10e90');
 
-INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', 'e6e1d5c3-0ed9-4478-aacd-bdfc3b0deaf2'); -- Marketing
-INSERT INTO category_service (category_id, service_id) VALUES ('7b8dcde3-50a1-4098-9382-03b61f5479cd', 'e6e1d5c3-0ed9-4478-aacd-bdfc3b0deaf2'); -- Administração
-INSERT INTO category_service (category_id, service_id) VALUES ('50ba3bb8-3b23-4d49-8f29-17af99c7403d', 'e6e1d5c3-0ed9-4478-aacd-bdfc3b0deaf2'); -- Contabilidade
+-- Service ID: c5eb8f24-0627-49ec-9ac7-d2c8b7ae78f3
+INSERT INTO category_service (category_id, service_id) VALUES ('1246f8d1-5bdf-4b9c-b7e4-49e3afc55ba8', 'f50c9904-a219-4f20-8b24-e54d05956cbd');
+INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', 'f50c9904-a219-4f20-8b24-e54d05956cbd');
+
+-- Service ID: 8e24d47f-0bd2-4b58-bf35-2fbd0e3c9ad1
+INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', '1586860f-19b5-4ba6-88f9-670346e23e33');
+INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', '1586860f-19b5-4ba6-88f9-670346e23e33');
+
+-- Service ID: 5d2f84a8-8739-4c1e-92a3-c38752b8b784
+INSERT INTO category_service (category_id, service_id) VALUES ('e8b123a3-4a29-4d47-bb1f-01c43e78f0d5', 'e06cd616-c9e2-4596-8e81-e0e4818bc39b');
+INSERT INTO category_service (category_id, service_id) VALUES ('7b8dcde3-50a1-4098-9382-03b61f5479cd', 'e06cd616-c9e2-4596-8e81-e0e4818bc39b');
+
+-- Service ID: 6a9f74e8-9824-4c3e-8f8b-d74c87ae29c8
+INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', '77ce5230-0202-4194-ba33-2922692da5e8');
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', '77ce5230-0202-4194-ba33-2922692da5e8');
+INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', '77ce5230-0202-4194-ba33-2922692da5e8');
+
+-- Service ID: 2c7d34b8-85ef-42e8-9a37-e67f98c4d3a1
+INSERT INTO category_service (category_id, service_id) VALUES ('47cb792b-75cf-470a-94fa-76be1a0298c4', 'e10611b9-0cea-424a-a49d-7400a5835c28');
+INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', 'e10611b9-0cea-424a-a49d-7400a5835c28');
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', 'e10611b9-0cea-424a-a49d-7400a5835c28');
+
+-- Service ID: 4e24c5b7-85b7-42e6-8b25-35c97ad5c3b8
+INSERT INTO category_service (category_id, service_id) VALUES ('bf34c6b0-5dcb-4c2d-8d2c-e90950e6e91f', '35432733-97c7-4f6e-8ccb-ce86d63d67c0');
+INSERT INTO category_service (category_id, service_id) VALUES ('6a6d98c3-45c1-4f64-9e3f-3e6d43c3f6c6', '35432733-97c7-4f6e-8ccb-ce86d63d67c0');
+
+-- Service ID: b5a9347c-3c58-47a9-859d-c5f47a23b9e7
+INSERT INTO category_service (category_id, service_id) VALUES ('cf6bc7cf-1c3a-4f85-ae80-6b90c20f7e95', '2d549783-6c56-44ea-8686-16db54290cf5');
+INSERT INTO category_service (category_id, service_id) VALUES ('de0f1b7c-5317-4b1f-8b1f-4cb23980d7c0', '2d549783-6c56-44ea-8686-16db54290cf5');
+INSERT INTO category_service (category_id, service_id) VALUES ('f3dcb593-5f36-4b4c-910f-ada5018c3e98', '2d549783-6c56-44ea-8686-16db54290cf5');
+
+-- Service ID: 34bc25a8-92f3-4b4c-93c7-27f5a39d5a8e
+INSERT INTO category_service (category_id, service_id) VALUES ('85c12d9e-0af9-4ad8-83a5-04677af15bde', '14fabce7-0b35-4df6-90e1-2f30c9aa9a56');
+INSERT INTO category_service (category_id, service_id) VALUES ('58d23991-7f10-4af3-8fcb-52a9a24697de', '14fabce7-0b35-4df6-90e1-2f30c9aa9a56');
+
+-- Service ID: 0f49c7b8-a24f-4e5a-9d72-c5e32b7f5d8e
+INSERT INTO category_service (category_id, service_id) VALUES ('dbd56a8d-8731-4094-9be3-5e824e9f7387', '6995359d-2185-458c-b5eb-e72dedbad621');
+
+
--- src/main/resources/db/imports/006_proposal_seed.sql
+++ src/main/resources/db/imports/006_proposal_seed.sql
@@ -1,25 +1,48 @@
--- Propostas para o serviço "Reforma de Escritório" do cliente Carla Oliveira
-INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time) VALUES
-                                                                                                                                 ('10b8a3e2-72a6-4b45-a841-90862cfed4a7', 'a9b4a2e7-f66c-4c89-88f2-59d66fefb6cb', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 'PENDING', 5500.00, 'Tenho experiência em reformas de escritório. Posso começar imediatamente.', NOW(), '2024-11-25'),
-                                                                                                                                 ('20c9b3f2-73b6-4b46-a942-90862cfed4b8', 'a9b4a2e7-f66c-4c89-88f2-59d66fefb6cb', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 'ACCEPTED', 6000.00, 'Já trabalhei em projetos semelhantes em São Paulo.', NOW(), '2024-11-30'),
-                                                                                                                                 ('30d0c4f3-74c6-4b47-a943-90862cfed4b9', 'a9b4a2e7-f66c-4c89-88f2-59d66fefb6cb', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 'REJECTED', 4800.00, 'Posso concluir com orçamento ajustado.', NOW(), '2024-11-28');
+-- Propostas para o serviço "Desenvolvimento de Website"
+INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time)
+VALUES ('c9a6e3b5-79b1-485a-bc8e-68b64c74a1f0', 'cfc26888-20d4-4146-83e5-d98ffc2d4642', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 'PENDING', 6500.00, 'Tenho experiência em desenvolvimento de websites responsivos e modernos.', NOW(), '2024-12-10');
 
--- Propostas para o serviço "Manutenção de Rede" do cliente Carla Oliveira
-INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time) VALUES
-                                                                                                                                 ('40e1d5f4-75d6-4b48-a944-90862cfed4b0', 'f2b72f3d-0f02-4b47-8a3c-b6c3d7c2f5e4', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 'PENDING', 2000.00, 'Experiência em manutenção de rede empresarial.', NOW(), '2024-11-20'),
-                                                                                                                                 ('50f2e6a5-76e6-4b49-a945-90862cfed4b1', 'f2b72f3d-0f02-4b47-8a3c-b6c3d7c2f5e4', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 'REJECTED', 1800.00, 'Posso atender no prazo solicitado.', NOW(), '2024-11-19');
+INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time)
+VALUES ('2f89a6bb-92d0-46db-b8b2-738f557fd529', 'cfc26888-20d4-4146-83e5-d98ffc2d4642', '003eced8-e3ae-4b24-b8d4-80cfc125151c', 'PENDING', 7000.00, 'Posso criar um website corporativo com funcionalidades avançadas.', NOW(), '2024-12-10');
 
--- Propostas para o serviço "Reforma de Escritório" do cliente Felipe Almeida
-INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time) VALUES
-                                                                                                                                 ('60g3f7b6-77f6-4b4a-a946-90862cfed4b2', '7c9c0c26-fc77-48f6-8729-754d5f3e5715', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 'PENDING', 5800.00, 'Posso realizar com qualidade e no prazo.', NOW(), '2024-11-29'),
-                                                                                                                                 ('70h4g8c7-78g6-4b4b-a947-90862cfed4b3', '7c9c0c26-fc77-48f6-8729-754d5f3e5715', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 'ACCEPTED', 5900.00, 'Experiência com reformas remotamente.', NOW(), '2024-11-30');
+-- Propostas para o serviço "Pintura Residencial"
+INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time)
+VALUES ('d324b5da-6d9c-47b5-8767-ff2b84b8be6c', 'dab38234-6f41-4ed7-b1b9-6227a8be7da6', 'a96253d5-424d-4f06-aa7d-dc6bf3002166', 'PENDING', 3500.00, 'Experiência em pintura interna com acabamento de alta qualidade.', NOW(), '2024-12-15');
 
--- Propostas para o serviço "Criação de Site" do cliente Felipe Almeida
-INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time) VALUES
-                                                                                                                                 ('80i5h9d8-79h6-4b4c-a948-90862cfed4b4', 'd6c8c60e-5d5c-4b3d-bfd9-c34c6ef06249', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 'PENDING', 2800.00, 'Trabalho com criação de sites profissionais.', NOW(), '2024-11-12'),
-                                                                                                                                 ('90j6i0e9-80i6-4b4d-a949-90862cfed4b5', 'd6c8c60e-5d5c-4b3d-bfd9-c34c6ef06249', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 'PENDING', 2500.00, 'Ofereço site funcional e otimizado.', NOW(), '2024-11-14');
+INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time)
+VALUES ('9c8f4976-fb88-47fb-804b-e5c2897bc7b3', 'dab38234-6f41-4ed7-b1b9-6227a8be7da6', '317201ee-0bc6-45f1-bbfc-058f2d2a318a', 'PENDING', 4000.00, 'Posso realizar a pintura com acabamentos personalizados e entrega no prazo.', NOW(), '2024-12-15');
 
--- Propostas para o serviço "Limpar Apartamento" do cliente Rafael Augusto
-INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time) VALUES
-                                                                                                                                 ('a1k7j1o0-81j6-4b4e-a950-90862cfed4b6', '4e2f1b42-3b43-4e4e-9c3b-1d2a4d6b76a9', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 'PENDING', 300.00, 'Limpeza eficiente e detalhada.', NOW(), '2024-10-14'),
-                                                                                                                                 ('b2l8k2p1-82k6-4b4f-a951-90862cfed4b7', '4e2f1b42-3b43-4e4e-9c3b-1d2a4d6b76a9', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 'ACCEPTED', 350.00, 'Disponível para a data.', NOW(), '2024-10-15');
+-- Propostas para o serviço "Manutenção de Rede"
+INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time)
+VALUES ('0e3b7a2f-3090-4642-bff6-b8c3803e5c63', 'ecef1ade-75a1-45c9-9140-1772a1a0dcf3', '003eced8-e3ae-4b24-b8d4-80cfc125151c', 'PENDING', 5000.00, 'Expertise em otimização e correção de redes internas empresariais.', NOW(), '2024-12-20');
+
+INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time)
+VALUES ('d699c6b7-df2d-4cfa-9a7e-2e86d6d1e58e', 'ecef1ade-75a1-45c9-9140-1772a1a0dcf3', 'a96253d5-424d-4f06-aa7d-dc6bf3002166', 'PENDING', 6500.00, 'Posso corrigir falhas e melhorar a performance da sua rede empresarial.', NOW(), '2024-12-20');
+
+-- Propostas para o serviço "Consultoria Financeira"
+INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time)
+VALUES ('f49827b9-e47c-4d63-9e2b-5313e6f2d8b0', '0937170f-329b-4be5-b660-2fc42751b280', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 'PENDING', 6000.00, 'Consultoria financeira para planejamento de longo prazo e investimentos.', NOW(), '2025-01-05');
+
+INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time)
+VALUES ('ceffadf3-9f7a-4b3b-8326-b16d6a25fa2f', '0937170f-329b-4be5-b660-2fc42751b280', '003eced8-e3ae-4b24-b8d4-80cfc125151c', 'PENDING', 7500.00, 'Assessoria em investimentos e gestão de fluxo de caixa para empresas.', NOW(), '2025-01-05');
+
+-- Propostas para o serviço "Reparo Elétrico"
+INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time)
+VALUES ('7b9e84d6-0ed7-41b0-82d9-5d6bc06b24b3', 'b5a81778-14f9-4b57-a8c5-ab9b71b8029c', 'a96253d5-424d-4f06-aa7d-dc6bf3002166', 'PENDING', 3000.00, 'Especialista em reparos elétricos e instalações seguras.', NOW(), '2024-12-25');
+
+INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time)
+VALUES ('f06b8cf1-f1d3-4baf-8263-f9e9c6cfb7a5', 'b5a81778-14f9-4b57-a8c5-ab9b71b8029c', '317201ee-0bc6-45f1-bbfc-058f2d2a318a', 'PENDING', 3500.00, 'Reparo elétrico e instalação de sistemas de iluminação modernos.', NOW(), '2024-12-25');
+
+-- Propostas para o serviço "Redação Publicitária"
+INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time)
+VALUES ('6f9b2d5d-d68b-4799-9f87-8817a2d0d7b5', '0ed04005-cc81-4032-92ce-4493447e1cec', 'a96253d5-424d-4f06-aa7d-dc6bf3002166', 'PENDING', 4500.00, 'Criação de conteúdos criativos para campanhas publicitárias e blogs.', NOW(), '2024-12-30');
+
+INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time)
+VALUES ('80a299f3-1069-4657-88a0-bc94e9a2cce3', '0ed04005-cc81-4032-92ce-4493447e1cec', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 'PENDING', 4000.00, 'Redação de textos publicitários e conteúdos voltados para vendas e conversão.', NOW(), '2024-12-30');
+
+-- Propostas para o serviço "Desenvolvimento de Aplicativo"
+INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time)
+VALUES ('23d4b64f-0a5c-4022-9b37-bf5b8b4732de', '0937170f-329b-4be5-b660-2fc42751b280', '003eced8-e3ae-4b24-b8d4-80cfc125151c', 'PENDING', 8000.00, 'Desenvolvimento de aplicativo para Android e iOS com integração com plataformas de pagamento.', NOW(), '2025-01-10');
+
+INSERT INTO proposal (id, service_id, freelancer_id, status, offer_amount, message, requested_at, estimated_completion_time)
+VALUES ('7201f440-c4db-4871-b9cf-f5642c178f61', '0937170f-329b-4be5-b660-2fc42751b280', 'a96253d5-424d-4f06-aa7d-dc6bf3002166', 'PENDING', 9000.00, 'Aplicativo mobile com funcionalidades avançadas de geolocalização e notificações.', NOW(), '2025-01-10');
