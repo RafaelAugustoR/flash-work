ccb55379caef28f1fe781923cddfbd0a4d6c33b4
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-11-23 17:40:50 -0300

    feat: seeding notification table

---
+++ src/main/resources/db/imports/012_notification_seed.sql
@@ -0,0 +1,19 @@
+INSERT INTO notification (id, content, date, is_viewed, notification_type, sender_id, receiver_id)
+VALUES
+    ('b3cfc0a2-7c3d-4f5a-84d7-96c5b4e12a1f', 'Bem-vindo ao sistema, Rafael!', '2024-11-23T10:00:00', FALSE, 'SYSTEM', '003eced8-e3ae-4b24-b8d4-80cfc125151c', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376');
+
+INSERT INTO notification (id, content, date, is_viewed, notification_type, sender_id, receiver_id)
+VALUES
+    ('e5d2a4b3-c5b1-4c87-84f3-9a7d5f82d3e9', 'Sua senha foi alterada com sucesso.', '2024-11-22T15:45:00', TRUE, 'SYSTEM', '003eced8-e3ae-4b24-b8d4-80cfc125151c', 'a96253d5-424d-4f06-aa7d-dc6bf3002166');
+
+INSERT INTO notification (id, content, date, is_viewed, notification_type, sender_id, receiver_id)
+VALUES
+    ('d82c7a5b-3e1f-4c7d-912b-4a7f5c9b27e4', 'Lembrete: Reunião às 14h.', '2024-11-20T09:00:00', FALSE, 'REMINDER', '317201ee-0bc6-45f1-bbfc-058f2d2a318a', 'fec2a3d3-350c-49d8-9c9c-47f5944800de');
+
+INSERT INTO notification (id, content, date, is_viewed, notification_type, sender_id, receiver_id)
+VALUES
+    ('a3f82c7b-5f4e-4b61-8d9c-3a7b5c21d9e7', 'Alerta: Saldo da carteira baixo.', '2024-11-21T18:30:00', TRUE, 'ALERT', '96c443ba-3336-44d0-b874-b03e87909cb0', 'd7062553-0a15-4ae1-9030-ab043c5c5a3c');
+
+INSERT INTO notification (id, content, date, is_viewed, notification_type, sender_id, receiver_id)
+VALUES
+    ('9c82f3a7-b4c8-4e3d-a7b5-f2c9d7e1a5b4', 'Bem-vindo ao sistema, Isabela!', '2024-11-23T11:00:00', FALSE, 'SYSTEM', '50c47e87-873a-4d82-8351-2b041123460a', '5868790a-1789-4ad8-95ef-7cc8b5419ff4');
