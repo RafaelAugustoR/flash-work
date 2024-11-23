c55cdda941cbe3d20e2a88cf37b42ac93396f676
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-11-23 17:35:27 -0300

    feat: seeding wallet table

---
+++ src/main/resources/db/imports/011_wallet_seed.sql
@@ -0,0 +1,32 @@
+INSERT INTO wallet (id, user_id, balance)
+VALUES ('cb7e67b9-a1d3-4b62-bb9e-d5f8e84d1af7', 'c7b3ef06-484e-4dc0-bd34-d1c7a0a4c376', 1000.00); -- Rafael Augusto
+
+INSERT INTO wallet (id, user_id, balance)
+VALUES ('82e1a2d4-7f23-4417-bc8e-1d1d3174a561', '003eced8-e3ae-4b24-b8d4-80cfc125151c', 2000.00); -- Alice Silva
+
+INSERT INTO wallet (id, user_id, balance)
+VALUES ('5a2c4623-6e91-4031-a8f2-e5c9712d3d6b', 'a96253d5-424d-4f06-aa7d-dc6bf3002166', 1500.00); -- Bruno Costa
+
+INSERT INTO wallet (id, user_id, balance)
+VALUES ('e4f87a9b-d53b-4cf7-b122-1c874c72b5a2', '317201ee-0bc6-45f1-bbfc-058f2d2a318a', 3000.00); -- Carla Oliveira
+
+INSERT INTO wallet (id, user_id, balance)
+VALUES ('b271dcf9-8723-4b72-b0cc-2d97cfd2e8d1', 'fec2a3d3-350c-49d8-9c9c-47f5944800de', 500.00); -- Daniel Pereira
+
+INSERT INTO wallet (id, user_id, balance)
+VALUES ('47ac342b-94f8-467a-8c27-83f1d234d7c4', '10caa2d5-22d9-4d4a-82d6-824ec304e288', 750.00); -- Eduarda Santos
+
+INSERT INTO wallet (id, user_id, balance)
+VALUES ('f8d237b4-5e7a-4c73-b8f3-21f48d6f94a6', '50c47e87-873a-4d82-8351-2b041123460a', 2500.00); -- Felipe Almeida
+
+INSERT INTO wallet (id, user_id, balance)
+VALUES ('6f3a28e7-d8b1-4501-99fc-7c3d62f9f6c2', '96c443ba-3336-44d0-b874-b03e87909cb0', 1200.00); -- Gisele Martins
+
+INSERT INTO wallet (id, user_id, balance)
+VALUES ('2c8b1d72-1e93-4c7f-8182-b8d2f9e3d627', 'd7062553-0a15-4ae1-9030-ab043c5c5a3c', 300.00); -- Henrique Lima
+
+INSERT INTO wallet (id, user_id, balance)
+VALUES ('98d1f283-4c51-43b7-b8c2-3e1f72d5f2a4', '5868790a-1789-4ad8-95ef-7cc8b5419ff4', 1800.00); -- Isabela Rocha
+
+INSERT INTO wallet (id, user_id, balance)
+VALUES ('5d83e742-3c71-4a98-87e2-d5b2c7f12a96', '19198073-7d10-4975-8e17-7c17e11c04de', 1300.00); -- João Ferreira
