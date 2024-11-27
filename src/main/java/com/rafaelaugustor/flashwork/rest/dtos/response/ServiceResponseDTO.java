cc2fa61d815efe96ba0f66a54fafbc50d31e2e3a
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-11-26 22:43:08 -0300

    feat: include field status in response

--- src/main/java/com/rafaelaugustor/flashwork/rest/dtos/response/ServiceResponseDTO.java
+++ src/main/java/com/rafaelaugustor/flashwork/rest/dtos/response/ServiceResponseDTO.java
@@ -1,5 +1,6 @@
 package com.rafaelaugustor.flashwork.rest.dtos.response;
 
+import com.rafaelaugustor.flashwork.domain.enums.ServiceStatus;
 import com.rafaelaugustor.flashwork.domain.enums.WorkType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,6 +27,7 @@ public class ServiceResponseDTO {
     private UserMinDTO freelancer;
     private Timestamp createdAt;
     private UserMinDTO client;
+    private ServiceStatus status;
     private List<CategoryResponseDTO> categories;
     private Integer proposalQuantity;
 }
