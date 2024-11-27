7102b2d74251177ea124d558c9889df44cb24cea
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-11-26 21:40:10 -0300

    feat: add @Builder in DigitalContractRequestDTO

--- src/main/java/com/rafaelaugustor/flashwork/rest/dtos/request/DigitalContractRequestDTO.java
+++ src/main/java/com/rafaelaugustor/flashwork/rest/dtos/request/DigitalContractRequestDTO.java
@@ -2,6 +2,7 @@ package com.rafaelaugustor.flashwork.rest.dtos.request;
 
 import com.rafaelaugustor.flashwork.rest.dtos.response.ServiceResponseDTO;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.UUID;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class DigitalContractRequestDTO {
     private UUID clientId;
     private UUID freelancerId;
