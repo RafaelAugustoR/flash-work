a298e0639a708aea7881b5d9a93d4e7199e29460
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-11-27 03:45:13 -0300

    feat: add query to DigitalContractController

--- src/main/java/com/rafaelaugustor/flashwork/repositories/DigitalContractRepository.java
+++ src/main/java/com/rafaelaugustor/flashwork/repositories/DigitalContractRepository.java
@@ -1,6 +1,9 @@
 package com.rafaelaugustor.flashwork.repositories;
 
 import com.rafaelaugustor.flashwork.domain.entities.DigitalContract;
+import com.rafaelaugustor.flashwork.domain.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,5 @@ import java.util.UUID;
 
 @Repository
 public interface DigitalContractRepository extends JpaRepository<DigitalContract, UUID> {
+    Page<DigitalContract> findByClientOrFreelancer(User client, User freelancer, Pageable pageable);
 }
