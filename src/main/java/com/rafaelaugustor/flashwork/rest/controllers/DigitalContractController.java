e8cc477e17b7995e746813b378e3d6d25776bbdf
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-11-27 02:10:53 -0300

    refactor: change endpoints

--- src/main/java/com/rafaelaugustor/flashwork/rest/controllers/DigitalContractController.java
+++ src/main/java/com/rafaelaugustor/flashwork/rest/controllers/DigitalContractController.java
@@ -8,28 +8,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.FileNotFoundException;
+import java.security.Principal;
 import java.util.UUID;
 
 import static com.rafaelaugustor.flashwork.utils.Constants.APP_ROOT;
 
 @RestController
-@RequestMapping(APP_ROOT + "/documents")
+@RequestMapping(APP_ROOT + "/contracts")
 @RequiredArgsConstructor
 @CrossOrigin
 public class DigitalContractController {
 
-    private final DigitalContractService documentGenerator;
+    private final DigitalContractService digitalContractService;
 
-    @PostMapping("/generate")
-    public void generateDocument(@RequestBody DigitalContractRequestDTO request) {
-        documentGenerator.generateDocument(request);
+    @PostMapping("/{serviceId}/generate")
+    public void generateDocument(@RequestBody DigitalContractRequestDTO request, @PathVariable UUID serviceId) {
+        digitalContractService.generateDocument(request, serviceId);
     }
 
-    @PostMapping("/sign")
-    public ResponseEntity<String> addSignatureToContract(@RequestParam UUID clientId,
-                                                         @RequestParam UUID freelancerId,
-                                                         @RequestBody SignatureRequestDTO signatureRequest) throws FileNotFoundException {
-        documentGenerator.addSignatureToContract(clientId, freelancerId, signatureRequest);
+    @PostMapping("/{contractId}/sign")
+    public ResponseEntity<String> addSignatureToContract(@PathVariable UUID contractId, Principal principal, @RequestBody SignatureRequestDTO signatureRequest) throws FileNotFoundException {
+        digitalContractService.addSignatureToContract(contractId, principal, signatureRequest);
         return ResponseEntity.ok("Assinatura adicionada com sucesso!");
     }
 }
