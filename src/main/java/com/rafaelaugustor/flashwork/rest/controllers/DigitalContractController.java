c679af9d87c0b90993d783bb361a5f8479247d3e
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-11-27 03:46:18 -0300

    feat: expose endpoints

--- src/main/java/com/rafaelaugustor/flashwork/rest/controllers/DigitalContractController.java
+++ src/main/java/com/rafaelaugustor/flashwork/rest/controllers/DigitalContractController.java
@@ -1,10 +1,11 @@
 package com.rafaelaugustor.flashwork.rest.controllers;
 
-import com.rafaelaugustor.flashwork.rest.dtos.request.DigitalContractRequestDTO;
 import com.rafaelaugustor.flashwork.rest.dtos.request.SignatureRequestDTO;
 import com.rafaelaugustor.flashwork.rest.dtos.response.DigitalContractResponseDTO;
 import com.rafaelaugustor.flashwork.services.DigitalContractService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,14 +23,21 @@ public class DigitalContractController {
 
     private final DigitalContractService digitalContractService;
 
-    @PostMapping("/{serviceId}/generate")
-    public ResponseEntity<DigitalContractResponseDTO> generateDocument(@RequestBody DigitalContractRequestDTO request, @PathVariable UUID serviceId) {
-        var contract = digitalContractService.generateDocument(request, serviceId);
-        return ResponseEntity.ok(contract);
-    }
-
     @PostMapping("/{contractId}/sign")
     public ResponseEntity<DigitalContractResponseDTO> addSignatureToContract(@PathVariable UUID contractId, Principal principal, @RequestBody SignatureRequestDTO signatureRequest) throws FileNotFoundException {
         return ResponseEntity.ok(digitalContractService.addSignatureToContract(contractId, principal, signatureRequest));
     }
+
+    @GetMapping("/{contractId}")
+    public ResponseEntity<DigitalContractResponseDTO> getContractById(@PathVariable UUID contractId) throws FileNotFoundException {
+        DigitalContractResponseDTO contract = digitalContractService.getContractById(contractId);
+        return  ResponseEntity.ok(contract);
+    }
+
+    @GetMapping("/user}")
+    public ResponseEntity<Page<DigitalContractResponseDTO>> getContractsByUser(Principal principal, Pageable pageable) {
+        Page<DigitalContractResponseDTO> contracts = digitalContractService.findContractsByUser(principal, pageable);
+        return  ResponseEntity.ok(contracts);
+    }
+
 }
