0036492ce6cb4ba8fb773f29fbbc555ee1c4e65d
RafaelAugustoR <rafaelaugustodev@gmail.com>
2024-11-26 22:40:03 -0300

    feat: add constructor in ServiceRequestDTO

--- src/main/java/com/rafaelaugustor/flashwork/rest/dtos/request/ServiceRequestDTO.java
+++ src/main/java/com/rafaelaugustor/flashwork/rest/dtos/request/ServiceRequestDTO.java
@@ -1,5 +1,7 @@
 package com.rafaelaugustor.flashwork.rest.dtos.request;
 
+import com.rafaelaugustor.flashwork.domain.entities.Category;
+import com.rafaelaugustor.flashwork.domain.entities.Service;
 import com.rafaelaugustor.flashwork.domain.enums.WorkType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,4 +24,16 @@ public class ServiceRequestDTO {
     private WorkType workType;
     private UUID addressId;
     private List<UUID> categories;
+
+    public ServiceRequestDTO(Service service) {
+        this.title = service.getTitle();
+        this.description = service.getDescription();
+        this.budget = service.getBudget();
+        this.deadline = service.getDeadline();
+        this.workType = service.getWorkType();
+        this.addressId = service.getAddressId();
+        for (Category category : service.getCategories()) {
+            this.categories.add(category.getId());
+        }
+    }
 }
